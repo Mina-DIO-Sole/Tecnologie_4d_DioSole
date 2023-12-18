bf275e718dbaa9a09efa2339ab7126c027f22110
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2023-12-18 08:00:13 +0000

    Description of Changes in this commit

---
+++ esercitazione13_14/src/app/fourth-component/fourth-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-component',
+  templateUrl: './fourth-component.component.html',
+  styleUrls: ['./fourth-component.component.css']
+})
+export class FourthComponentComponent {
+    title = 'esercitazione13_14';
+    nome= "quarto";
+}
