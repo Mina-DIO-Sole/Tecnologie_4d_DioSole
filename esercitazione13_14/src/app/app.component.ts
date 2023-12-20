061831c8d2c577e1c0764eb7041595470965d448
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2023-12-20 12:21:04 +0000

    Description og Changes in this commit

--- esercitazione13_14/src/app/app.component.ts
+++ esercitazione13_14/src/app/app.component.ts
@@ -4,8 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
   title = 'esercitazione13_14';
   nome= "app";
+  CONTANTI = [
+    {id: 1, name:'Soldi Spocrchi'},
+    {id: 2, name:'dollari'},
+    {id: 5, name:'euro'},
+    {id: 3, name:'lira egiziana'},
+    {id: 4, name:'soldi puliti'}
+
+
+  ];
+
 }
+
