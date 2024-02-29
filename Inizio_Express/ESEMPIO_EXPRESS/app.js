79edb4b746c0bc2a4f79539119f0657552d78345
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-02-29 07:54:48 +0000

    faty

--- Inizio_Express/ESEMPIO_EXPRESS/app.js
+++ Inizio_Express/ESEMPIO_EXPRESS/app.js
@@ -1,8 +1,13 @@
+var express = require('express');
+var app = express(); 
 const path = require('path');
 //assuming app is express Object.
+
 app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
- });
+ res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
