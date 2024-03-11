40ffa75d5bd04e59a0c3a8fd5c414ef323a65f40
Mina-DIO-Sole Soliman <soliman.mina@iisgalvanimi.edu.it>
2024-03-11 21:06:42 +0000

    va il trasferimento da about a sitemamp a index

--- Inizio_Express/ESEMPIO_EXPRESS/app.js
+++ Inizio_Express/ESEMPIO_EXPRESS/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express(); 
 const path = require('path');
-//assuming app is express Object.
 
-app.get('/about',function(req,res){
- res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
+app.get('/',function(req,res){
+ res.sendFile(path.join(__dirname, 'index.html')); //__dirname : Ritorna la cartella del progetto
 });
-
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname,'about.html')); //__dirname : Ritorna la cartella del progetto
+ });
+ app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname,'sitemap.html')); //__dirname : Ritorna la cartella del progetto
+ });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
